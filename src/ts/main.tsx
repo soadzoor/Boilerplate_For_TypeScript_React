761d1fc8cc17c1443562c6548e7ad831f39cc61a
Peter Varga <soadzoor@gmail.com>
2022-05-06 12:18:24 +0200

    Upgrade libs

--- src/ts/main.tsx
+++ src/ts/main.tsx
@@ -1,12 +1,13 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as ReactDOM from "react-dom/client";
 import App from "./App";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("App")!)
+
+root.render(
 	<React.StrictMode>
 		<App />
-	</React.StrictMode>,
-	document.getElementById("App")
+	</React.StrictMode>
 );
 
 // eslint-disable-next-line import/no-unused-modules
