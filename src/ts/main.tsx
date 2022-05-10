3d2188cefc7615312a92176493e269f743d86459
Peter Varga <soadzoor@gmail.com>
2022-05-10 14:06:00 +0200

    Add new eslint rules for member-delimiter-style

--- src/ts/main.tsx
+++ src/ts/main.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("App")!)
+const root = ReactDOM.createRoot(document.getElementById("App")!);
 
 root.render(
 	<React.StrictMode>
